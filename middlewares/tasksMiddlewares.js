28967123ca3ca960b3793bd78ed73a05d6217ece
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-11-04 17:05:58 -0300

    refatora middleware de verificação

--- middlewares/tasksMiddlewares.js
+++ middlewares/tasksMiddlewares.js
@@ -4,16 +4,9 @@ const validateName = (req, res, next) => {
   next();
 };
 
-const validStatus = ['pendente', 'em andamento', 'pronto'];
-
 const validateStatus = (req, res, next) => {
   const { status } = req.body;
   if (!status) return res.status(422).json({ message: 'Este campo é obrigatório' });
-  if (!validStatus.includes(status)) {
-    return res.status(422).json({
-      message: 'O status deve ser "pendente", "em andamento" ou "pronto"',
-    });
-  }
   next();
 };
 
