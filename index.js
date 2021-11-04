1f472327e73e80d95be23df34602ac097f122643
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-11-03 22:25:39 -0300

    Ajustes no dotenv

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
--- models/connection.js
+++ models/connection.js
@@ -6,8 +6,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = process.env.localhost || process.env.MONGO_DB_URL;
-const DB_NAME = 'ToDoList' || process.env.DB_NAME;
+const MONGO_DB_URL = process.env.MONGO_DB_URL || process.env.localhost;
+const DB_NAME = process.env.DB_NAME || 'ToDoList';
 
 let db = null;
 
